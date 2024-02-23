4871c453905c3f3e713321606c7e798c5e6abb03
ng1n3 <code.with.muyiwa@gmail.com>
2024-02-23 09:06:46 +0100

    currency feature implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,56 @@
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [output, setOutput] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCurrency() {
+      try {
+        setLoading(true)
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+        );
+        const data = await res.json()
+        setOutput(data.rates[toCurrency]);
+        setLoading(false)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    if(fromCurrency === toCurrency) return setOutput(amount.toString())
+    fetchCurrency()
+  }, [amount, toCurrency, fromCurrency]);
   return (
     <div>
-      Hello world!
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={loading}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+        disabled={loading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+      </select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+        disabled={loading}
+      >
+        <option value="EUR">EUR</option>
+        <option value="USD">USD</option>
+        <option value="CAD">CAD</option>
+      </select>
+      <p>{output} {toCurrency}</p>
     </div>
   );
 }
